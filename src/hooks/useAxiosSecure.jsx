dba5cf88c8f0816e092894110fcf52ca11da92a2
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2026-05-05 13:44:51 +0600

    added responseinterceptor,handled 401,403 to logout user for bad request

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import useAuth from "./useAuth";
 import { useEffect } from "react";
+import { useNavigate } from "react-router";
 
 const axiosInstance = axios.create({
     baseURL: "http://localhost:3000",
@@ -8,7 +9,8 @@ const axiosInstance = axios.create({
 
 const useAxiosSecure = () => {
 
-    const { user } = useAuth();
+    const { user, logOut } = useAuth();
+    const navigate = useNavigate();
 
     useEffect(() => {
         //request interceptor
@@ -16,11 +18,28 @@ const useAxiosSecure = () => {
             config.headers.authorization = `Bearer ${user.accessToken}`;
             return config;
         })
+
+
+        //response interceptor
+        const responseInterceptor = axiosInstance.interceptors.response.use(res => {
+            return res;
+        }, err => {
+            console.log("error object", err);
+            if (err.status === 401 || err.status === 403) {
+                logOut()
+                    .then(() => {
+                        navigate("/login");
+                    })
+            }
+            return Promise.reject(err);
+        })
+
         return () => {
             axiosInstance.interceptors.request.eject(requestInterceptor)
+            axiosInstance.interceptors.response.eject(responseInterceptor)
         }
 
-    }, [user])
+    }, [user,logOut,navigate])
 
     // axiosInstance.interceptors.request.use((config) => {
     //     // console.log(config);
